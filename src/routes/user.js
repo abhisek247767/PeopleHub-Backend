ed429ae526838bde636222273547bb9b80c94adc
KanishkChhajed <kanishk.chhajed@gmail.com>
2025-09-25 10:25:19 +0530

    No Extra Add

--- src/routes/user.js
+++ src/routes/user.js
@@ -13,10 +13,6 @@ router.get('/departments', fetchDepartment);
 router.get('/subDepartment', fetchSubDepartment);
 
 // Profile Picture Upload and Retrieval
-<<<<<<< HEAD
-router.get('/profile-picture/:userId', getProfilePicture);v
-=======
->>>>>>> 349c80d2ff95efe905f55a9179b3646136f6a64a
 router.put('/update-profile', authenticate(), updateProfile);
 
 router.get('/profile-picture/:userId', getProfilePicture);
